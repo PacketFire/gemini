e11edcb984175ef9beb2ab3183a8416638ea28d1
sabinprj <sabin.parajuli13@gmail.com>
2018-10-30 21:22:37 +0545

    feat: Added initial setup.py and updated README including installation step
    Maybe closes issue #2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gemini
 
+## Installation
+* Clone this project
+* Create a virtualenv
+* Run the command below
+```shell
+  $ pip install --editable .
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="gemini",
+    version="0.1",
+    description="A simple container scheduler",
+    author="PacketFire",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: DevOps",
+        "Topic :: Containers :: Scheduler",
+        "Programming Language :: Python :: 3",
+    ],
+
+    keywords="gemini scheduler container",
+    packages=["gemini"],
+
+    install_requires=[]
+)
