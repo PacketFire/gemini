addd83d6fa1514a73d2feb127e8358be79b449fb
Brock Ramsey <brockramz@gmail.com>
2018-11-04 01:31:22 -0500

    Add info endpoint (#32)
    * Add info endpoint
    
    * Move info endpoint to metadata package
    
    * Fix function name call
    
    * Update metadata and route
    
    * Fix missing function name
    
    * Fix function return type
    
    * Update return type and data output

--- gemini/master.py
+++ gemini/master.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+import json
+import metadata
 
 app = Flask('gemini-master')
 
@@ -8,6 +9,12 @@ app = Flask('gemini-master')
 def home() -> str:
     return "Home"
 
+@app.route('/_info')
+def info() -> str:
+    response = {
+        "version": json.dumps(metadata.version())
+    }
+    return str(response)
 
 def start_master_server() -> None:
     app.run(debug=True)
---
+++ gemini/metadata.py
@@ -0,0 +1,4 @@
+VERSION = "v0.0.1"
+
+def version():
+    return VERSION
