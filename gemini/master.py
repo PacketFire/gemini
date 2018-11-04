fa72a2fc094ea10ade576280a028349449e38454
bramz <brockramz@gmail.com>
2018-11-04 00:45:46 -0400

    Add info endpoint

--- gemini/master.py
+++ gemini/master.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import json
 
-
+VERSION = "0.1"
 app = Flask('gemini-master')
 
 
@@ -8,6 +9,18 @@ app = Flask('gemini-master')
 def home() -> str:
     return "Home"
 
+@app.route('/_info')
+def info() -> str:
+    body = {
+        "version":  VERSION 
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
 
 def start_master_server() -> None:
     app.run(debug=True)
