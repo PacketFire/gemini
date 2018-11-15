99a9b0960d47cc5b7f4b5086394621066a46faa9
bramz <brockramz@gmail.com>
2018-11-15 14:14:13 -0500

    Add ping call from node to master

--- gemini/node.py
+++ gemini/node.py
@@ -1,3 +1,15 @@
+import sched, time, requests
 
 def start_node() -> None:
     print('starting node')
+    sched_handler()
+
+def ping_master() -> str:
+    r = requests.post("http://localhost:5000/v1/nodes/ping")
+
+    return r.status_code
+
+def sched_handler() -> None:
+    s = sched.scheduler(time.time, time.sleep)
+
+    s.enter(30, 1, ping_master())
