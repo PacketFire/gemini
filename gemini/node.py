e1e730c04cecd86f92bd091c2db4391eec91241d
Brock Ramsey <brockramz@gmail.com>
2018-11-16 01:30:13 -0500

    Feature/44/node ping call (#48)
    * Add ping call from node to master
    
    * Update scheduler function
    
    * Refactor for asyncio use
    
    * Update to pass headers

--- gemini/node.py
+++ gemini/node.py
@@ -1,3 +1,21 @@
+import asyncio, time, requests
 
 def start_node() -> None:
     print('starting node')
+
+    loop = asyncio.get_event_loop()
+    try:
+        asyncio.ensure_future(ping_process())
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+
+async def ping_process() -> None:
+    url = 'http://localhost:5000/v1/nodes/ping'
+    headers = {'Authorization': 'dummy'}
+
+    while True:
+        await asyncio.sleep(30)
+        r = requests.post(url, headers=headers)
