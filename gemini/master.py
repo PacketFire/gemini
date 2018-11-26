8ca81349da5b04ba1d017c9da98d9f17301deab8
Brock Ramsey <brockramz@gmail.com>
2018-11-25 19:28:31 -0500

    Node authentication (#52)
    * Update for pw matching & token generation
    
    * Update route for proper authentication
    
    * Fixed to add node id to request body

--- gemini/master.py
+++ gemini/master.py
@@ -34,11 +34,11 @@ def info() -> str:
 def join() -> str:
     node_id = generate_node_id()
     password = generate_node_password()
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
     value = {
         'node_id': node_id,
-        'password': hashed,
+        'password': hashed.decode('utf8'),
     }
 
     c.kv.put('nodes/' + node_id, json.dumps(value))
@@ -49,6 +49,23 @@ def join() -> str:
     }
 
     return jsonify(response)
+
+
+@app.route('/v1/nodes/auth', methods=['POST'])
+def auth() -> str:
+    data = request.get_json()
+    cdata = c.kv.get('nodes/' + data['node_id'])
+    cvalues = json.loads(cdata[1]['Value'])
+
+    if bcrypt.checkpw(data['password'].encode('utf8'), cvalues['password'].encode('utf8')):
+        token = generate_node_token()
+        response = {
+            'token': token,
+        }
+        return jsonify(response)
+    else:
+        return "Passwords do not match\n"
+
 
 @app.route('/v1/nodes/ping', methods=['POST'])
 def ping() -> str:
@@ -69,10 +86,8 @@ def refresh() -> str:
 
     return jsonify(response)
 
-
 def generate_node_id() -> str:
     return secrets.token_hex(NODE_ID_BYTES)
-
 
 def generate_node_token() -> str:
     return secrets.token_hex(NODE_TOKEN_BYTES)
