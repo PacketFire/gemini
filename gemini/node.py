8f22683db68557d4dde2260ad5c86133d77ee45e
bramz <brockramz@gmail.com>
2018-11-28 07:39:19 +0000

    Add Nodefile class for flatfile persistence

--- gemini/node.py
+++ gemini/node.py
@@ -1,4 +1,23 @@
-import asyncio, time, requests
+import asyncio, time, requests, json
+
+class Nodefiles:
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def reader(self):
+        try:
+            self.fh = open("data/" + self.filename, "r")
+        except IOError:
+            print("file does not exist")
+
+    def writer(self, data):
+        try:
+            with open("data/" + self.filename, "r") as self.fh:
+                self.fh.write(json.dumps(data))
+        except IOError:
+            with open("data/" + self.filename, "w") as self.fh:
+                self.fh.write(json.dumps(data))
+
 
 def start_node() -> None:
     print('starting node')
@@ -50,8 +69,15 @@ def auth_node(node_id, password) -> None:
 
     try:
         response = r.json()
+        
+        nf = Nodefiles(node_id)
+        filedata = {
+            'node_id': node_id,
+            'password': password,
+            'token': response['token'],
+        }
+        nf.writer(filedata)
+
         print(response['token'])
     except ValueError:
         print("no token returned, credentials do not validate.")
-    
-    
