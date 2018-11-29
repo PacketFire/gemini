721674147c6559d96773bd5c1545806badc11e04
bramz <brockramz@gmail.com>
2018-11-29 03:12:28 +0000

    Refactor class and file handling

--- gemini/node.py
+++ gemini/node.py
@@ -1,31 +1,5 @@
 import asyncio, time, requests, json
 
-class Nodefile:
-    def __init__(self, filename):
-        self.filename = filename
-    
-    def reader(self):
-        try:
-            self.fh = open("data/" + self.filename, "r")
-        except IOError:
-            print("file does not exist for node")
-
-    def writer(self, data):
-        try:
-            with open("data/" + self.filename, "r") as self.fh:
-                self.fh.write(json.dumps(data))
-        except IOError:
-            with open("data/" + self.filename, "w") as self.fh:
-                self.fh.write(json.dumps(data))
-
-    def exists(self) -> bool:
-        try:
-            self.fh = open("data/" + self.filename, "r")
-            return True
-        except IOError:
-            return False
-
-
 def start_node() -> None:
     print('starting node')
     join_master()
@@ -76,15 +50,58 @@ def auth_node(node_id, password) -> None:
 
     try:
         response = r.json()
-
-        nf = Nodefile('node.json')
+        
         filedata = {
             'node_id': node_id,
             'password': password,
         }
-        nf.writer(filedata)
+        node_file_writer(filedata)
 
         print(response['token'])
     except ValueError:
-        print("no token returned, credentials do not validate.")
+        print('no token returned, credentials do not validate.')
 
+
+def node_file_reader() -> dict:
+    try:
+        with open('data/node.json', 'r') as fh:
+            data = json.load(fh)
+            return data
+    except IOError:
+        return 'json file does not exist for node'
+
+
+def node_file_writer(data):
+    try:
+        with open('data/node.json', 'r') as fh:
+            fh.write(json.dumps(data))
+    except IOError:
+        with open('data/node.json', 'w') as fh:
+            fh.write(json.dumps(data))
+
+
+def node_file_exists() -> bool:
+    try:
+        fh = open('data/node.json', 'r')
+        fh.close()
+        return True
+    except IOError:
+        return False
+
+
+class Nodedata:
+    def __init__(self, node_id, password):
+        self.node_id = node_id
+        self.password = password
+
+    def get_node_id(self) -> str:
+        return self.node_id
+
+    def set_node_id(self, newid):
+        self.node_id = newid
+
+    def get_node_password(self) -> str:
+        return self.password
+
+    def set_node_password(self, newpass):
+        self.password = newpass
