9083b5d696264dcf48dc5ec6013d9fe5bb766762
Raas Ahsan <agro@jantox.com>
2018-11-30 18:13:44 -0600

    cleanup

--- gemini/master.py
+++ gemini/master.py
@@ -1,11 +1,13 @@
 import json
 import secrets
+
 import bcrypt
 import consul
 import metadata
 from flask import Flask
 from flask import jsonify
 from flask import request
+
 
 NODE_ID_BYTES = 8
 NODE_TOKEN_BYTES = 32
@@ -53,11 +55,14 @@ def join() -> str:
 
 @app.route('/v1/nodes/auth', methods=['POST'])
 def auth() -> str:
-    data = request.get_json()
-    cdata = c.kv.get('nodes/' + data['node_id'])
-    cvalues = json.loads(cdata[1]['Value'])
+    body = request.get_json()
+    node_entry = c.kv.get('nodes/' + body['node_id'])
+    node_data = json.loads(node_entry[1]['Value'])
 
-    if bcrypt.checkpw(data['password'].encode('utf8'), cvalues['password'].encode('utf8')):
+    if bcrypt.checkpw(
+        body['password'].encode('utf8'),
+        node_data['password'].encode('utf8'),
+    ):
         token = generate_node_token()
         response = {
             'token': token,
@@ -86,14 +91,18 @@ def refresh() -> str:
 
     return jsonify(response)
 
+
 def generate_node_id() -> str:
     return secrets.token_hex(NODE_ID_BYTES)
+
 
 def generate_node_token() -> str:
     return secrets.token_hex(NODE_TOKEN_BYTES)
 
+
 def generate_node_password() -> str:
     return secrets.token_hex(NODE_PASSWORD_BYTES)
+
 
 def start_master() -> None:
     app.run(debug=True)
